7365a58ce9cde8bb447a9f7486922da0162b669c
Ronak Vithlani <30650520+imrkv@users.noreply.github.com>
2017-08-03 21:37:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/first-article', function (req , res) {
+   res.send('First article is requested and served'); 
+});
+
+app.get('/first-article', function (req , res) {
+   res.send('First article is requested and served'); 
+});
+
+app.get('/first-article', function (req , res) {
+   res.send('First article is requested and served'); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
