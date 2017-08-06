8672e5d2054af83f30b06c0188d460b888afab91
Ronak Vithlani <30650520+imrkv@users.noreply.github.com>
2017-08-06 11:40:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,8 +85,8 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var counter=0;
-app.get('/counter',function(req,res){
-    counter+=1;
+app.get('/counter', function (req , res) {
+    counter = counter + 1;
     res.send(counter.toString());
 });
 
