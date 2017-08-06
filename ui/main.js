39da4cc2de788b81384ff9c9d36c6b8a5c429cc8
Ronak Vithlani <30650520+imrkv@users.noreply.github.com>
2017-08-06 15:28:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,3 +16,29 @@ button.onclick = function() {
     request.open('GET', 'http://rkvithlani.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+var submit = document.getElementById('submit_btn');
+button.onclick = function() {
+    //Create a Request to counter Endpoint
+    var request = new XMLHttpRequest();
+    
+    //capture response and store it in variable
+    request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE && request.status === 200 ){
+            var names=request.responseText;
+            names=JSON.parse(names);
+            var list='';
+            for(i=0;i<names.length;i++){
+                list = "<li>" + names[i] + "</li>"
+                }
+            var ul=document.getElementById('namelist');
+            ul.innerHTML=list;
+        }  
+    };
+
+    //Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET', 'http://rkvithlani.imad.hasura-app.io/submit-name?name=' + name ,true);
+    request.send(null);
+};
