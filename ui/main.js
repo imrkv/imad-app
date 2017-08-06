01c877a46df49c14406e3a06f4d2bbc885abe57d
Ronak Vithlani <30650520+imrkv@users.noreply.github.com>
2017-08-06 14:49:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,20 @@
-var button = document.getElementById("counter");
-button.onclick = function(){
+var button = document.getElementById('counter');
+button.onclick = function() {
     //Create a Request to counter Endpoint
     var request = new XMLHttpRequest();
     
     //capture response and store it in variable
-    request.onreadystatechange = function(){
-      if(request.readyState ===XMLHttpRequest.DONE ){
-            if(request.status===200){
+    request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE ){
+            if(request.status === 200){
                 var counter=request.responseText;
                 var span=document.getElementById('count');
                 span.innerHTML=counter.toString();
             }   
       }  
     };
-};
+
     //Make the request
     request.open('GET', 'http://rkvithlani.imad.hasura-app.io/counter',true);
     request.send(null);
+};
