505767c4077ee2fa127302cdfdf3a0796f977e91
Ronak Vithlani <30650520+imrkv@users.noreply.github.com>
2017-08-18 13:02:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,12 +78,11 @@ function createTemplate(data){
 
 var pool = new Pool(config);
 app.get('/test-db',function(req,res) {
-   pool.query("SELECT * FROM test",function(err,result){
+   pool.query('SELECT * FROM test',function(err,result){
       if(err) {
           res.status(500).send(err.toString());
-      }
-      else{
-          res.send(JSON.stringify(result.row));
+      }else {
+          res.send(JSON.stringify(result.rows));
       }
    }); 
 });
