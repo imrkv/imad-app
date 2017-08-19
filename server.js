5500f88668e0b11bae6d97fae41ebf9873fd6304
Ronak Vithlani <30650520+imrkv@users.noreply.github.com>
2017-08-19 21:39:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var app = express();
 app.use(morgan('combined'));
@@ -79,6 +80,17 @@ function createTemplate(data){
   `;
   return htmlTemplate;
 }
+
+function hash(input,salt){
+    var hashed = crypto.pbkdf2Sync(input, 'salt', 10000, 512, 'sha512');
+    return hashed.toString('HEX');
+}
+
+app.get('/hash/:input',function(req,res){
+   var hashedString = hash(req.params.input,'this-is-some-random-string'); 
+});
+
+
 
 var pool = new Pool(config);
 app.get('/test-db',function(req,res) {
