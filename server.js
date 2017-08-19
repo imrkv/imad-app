1121a978fd6adb27bdd41ca7d37e878c9cf2445e
Ronak Vithlani <30650520+imrkv@users.noreply.github.com>
2017-08-19 22:22:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = reqire('body-parser');
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 var counter=0;
 app.get('/counter', function (req , res) {
@@ -91,7 +93,19 @@ app.get('/hash/:input',function(req,res){
    res.send(hashedString);
 });
 
-
+app.post('/create-user', function(req,res){
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('HEX');
+   var dbString = hash(password,salt);
+   pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString], function(err,result){
+       if(err) {
+          res.status(500).send(err.toString());
+      }else {
+          res.send("user Successfully Created" + username);
+      }
+   });
+});
 
 var pool = new Pool(config);
 app.get('/test-db',function(req,res) {
