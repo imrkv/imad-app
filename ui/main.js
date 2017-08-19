2bf1a8b634890a12083566b43fc1586bf48fad04
Ronak Vithlani <30650520+imrkv@users.noreply.github.com>
2017-08-19 22:52:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+/*
 var button = document.getElementById('counter');
 button.onclick = function() {
     //Create a Request to counter Endpoint
@@ -41,4 +42,31 @@ submit.onclick = function() {
     var name = nameInput.value;
     request.open('GET', 'http://rkvithlani.imad.hasura-app.io/submit-name?name=' + name ,true);
     request.send(null);
+};*/
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    //Create a Request to counter Endpoint
+    var request = new XMLHttpRequest();
+    
+    //capture response and store it in variable
+    request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE && request.status === 200 ){
+            var names=request.responseText;
+            names=JSON.parse(names);
+            var list='';
+            for(var i=0;i<names.length;i++){
+                list += "<li>" + names[i] + "</li>";
+                }
+            var ul=document.getElementById('namelist');
+            ul.innerHTML=list;
+        }  
+    };
+
+    //Make the request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    request.open('POST', 'http://rkvithlani.imad.hasura-app.io/login' ,true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
 };
