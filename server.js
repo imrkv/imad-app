d93a1ba9ad1b1ada9821d7561c6a06426e3d9cac
Ronak Vithlani <30650520+imrkv@users.noreply.github.com>
2017-08-28 10:49:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,7 +132,8 @@ app.post('/login', function(req,res){
               if(hashedPassword === dbString){
                   
                   req.session.auth = {userId: result.rows[0].id};
-                  res.send('Credential Correct');
+                  res.status(200).send(null);
+                  //res.send('Credential Correct');
               }
               else{
                   res.status(403).send('invalid username/password');
