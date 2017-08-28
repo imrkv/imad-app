baca604dbd53d3abdc65d386615bf6888e324f49
Ronak Vithlani <30650520+imrkv@users.noreply.github.com>
2017-08-28 13:08:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ app.post('/login', function(req,res){
                   
                   req.session.auth = {userId: result.rows[0].id};
                   res.setHeader('Content-Type', 'application/json');
-                  res.send(JSON.stringify('Credential Correct'));
+                  res.send(JSON.stringify('1:Credential Correct'));
               }
               else{
                   res.status(403).send('invalid username/password');
