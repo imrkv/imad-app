4f192b1a1f163cf5aacc71999415461ef7044479
Ronak Vithlani <30650520+imrkv@users.noreply.github.com>
2017-08-30 20:27:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -211,8 +211,7 @@ app.get('/get-articles',function(req,res){
            if(result.rows.length === 0) {
                 res.status(404).send(JSON.parse('{"error":"Article Not Found"}'));
             }else{
-                for(var i=0; i<=result.rows.lenght;i++)
-                    res.send(result.rows[i]);
+                res.send(result.rows);
             }
        }
    }); 
